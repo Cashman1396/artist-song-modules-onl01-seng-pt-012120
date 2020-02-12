887b0c4738f591a9aa21a1a65535acbc62a6bfe6
Chris Ashman <chrisashman96@gmail.com>
2020-02-12 22:10:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,14 +23,6 @@ class Song
     @@songs
   end
 
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
-
   def artist=(artist)
     @artist = artist
     artist.add_song(self) unless artist.song.include?(self)
